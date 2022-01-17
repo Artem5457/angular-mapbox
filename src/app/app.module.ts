1bda138d714ed903254ccefd3738fba570062269
Artem Bolshak <Artem.Bolshak@mht.priv>
2022-01-17 15:28:24 +0200

    solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgxMapboxGLModule} from "ngx-mapbox-gl";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA', // Optional, can also be set per map (accessToken input of mgl-map)
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
